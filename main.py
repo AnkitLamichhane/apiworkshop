988aefd945ae7debbedcb547d8a1533493696677
Ankeet <lamichhaneankit124@gmail.com>
2022-07-16 15:54:56 +0545

    added user to create API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+# from .models import Person
 from sqlalchemy.orm import Session
 import database
 import models
@@ -11,3 +12,22 @@ app = FastAPI()
 def get_user(id: int, db: Session = Depends(database.get_db)):
     data = db.query(models.student).filter(models.student.id == id).first()
     return {"data": data}
+
+     #127.0.0.1:8000/person
+
+#postmethod is used to add data in database 
+@app.post("/person")
+def add_person( username:str, password : str,email :str, blog_id:int, db: Session=Depends(database.get_db)):
+ data= models.Person(person_username = username, person_password = password, person_email = email, blog_id = blog_id )
+ db.add(data)  #database mah lagera data lai add gareko
+ db.commit()
+ return{"status":201,"message": "Data Added Successfully","data":data}
+
+#getmethod is use to read data 
+ @app.get("/person/{id}")
+ def get_person_by_id(id: int , db: Session = Depends(database.get_db)):
+    data = db.query(models.Person).filter(models.Person.person_id == id).first()
+    return {"status":200 , "message": "Data Found", "data": data}
+
+
+
--- models.py
+++ models.py
@@ -12,4 +12,13 @@ class student(Base):
     dob=Column('dob' ,NullType)
     #table sanga connect gareko i.e table ko kura lai api sanga link gareko
 
+class Person(Base): #database vitra ko table ko information leko 
+        __tablename__ = 'table_person'
+        person_id = Column(Integer,primary_key = True, index = True)
+        person_username = Column ('username',NullType)  #note: string name ra table vitra ko headline ko name same huna parxa
+        person_password = Column('password',NullType)
+        person_email = Column('email',NullType)
+        blog_id = Column('blog_id',NullType)
+
+
 
